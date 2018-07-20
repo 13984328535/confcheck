458707ff7954ae8db188f14e5e71ab4976f083d2
JayTan <JayTan@windows10.microdone.cn>
2018-07-20 14:34:20 +0800

    任务调整

--- home_application/celery_tasks.py
+++ home_application/celery_tasks.py
@@ -41,9 +41,10 @@ def async_task_load_app_config():
     """
     now = datetime.datetime.now()
     logger.info(u"async_task_load_app_config 定时任务加载应用配置数据：{}".format(now))
-    apps = APPConfig.objects.all()
     global apps1
-    apps1 = apps
+    apps1 = APPConfig.objects.all()
+    
+    #apps1 = apps
     print apps1
     #cache.__delattr__("V_CACHE_APPS")
     #cache.__setattr__("V_CACHE_APPS", apps)
