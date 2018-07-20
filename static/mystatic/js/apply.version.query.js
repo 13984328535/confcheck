995687ef9da236cdd7c0dd03bd291b35d452e777
JayTan <JayTan@windows10.microdone.cn>
2018-07-20 09:46:46 +0800

    界面变更

--- static/mystatic/js/apply.version.query.js
+++ static/mystatic/js/apply.version.query.js
@@ -61,9 +61,9 @@ function getInfo(id,name,result){
 					layer.msg(returnData.text,{icon: 2,time: 6000});
 				}*/
 				if(returnData.code){
-		  			var con="<div style='font-size:14px;margin-left:15px;margin-top:8px;'><b>"+returnData.text+"</b></div>"+
+		  			var con="<div style='font-size:14px;margin-left:15px;margin-top:8px;width:500px'><b>"+returnData.text+"</b></div>"+
 			  		"<div style='font-size:14px;margin-left:15px;margin-top:8px;'><b>IP:<span style='color:red'>"+returnData.IP+"</span></b></div>"+
-			  		"<div style='font-size:14px;margin-left:15px;margin-top:8px;'><b>路径:<span style='color:red'>"+returnData.path+"</span></b></div>";
+			  		"<div style='font-size:14px;margin-left:15px;margin-top:8px;width:500px'><b>路径:<span style='color:red'>"+returnData.path+"</span></b></div>";
 					var index = parent.layer.open({
 						type: 1,
 						title: name+'  提取结果',
